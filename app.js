5b6298340c0d2687bd84ae641bde6e4b5a20b492
Keithkiarie <keithkiarie@gmail.com>
2023-12-23 14:55:38 +0300

    swagger

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
 // Database
 const db = require("./config/mysql");
 
@@ -20,6 +23,30 @@ app.use(bodyParser.json());
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+const PORT = process.env.PORT || 4000;
+
+// swagger set up
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Ecommerce',
+      version: '1.0.0',
+      description: 'Ecommerce site',
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+      },
+    ],
+  },
+  apis: [path.join(__dirname, "routes", "*.js")],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
 // ROUTES
 app.use("/api/cart", require("./routes/cart"));
 app.use("/api/category", require("./routes/category"));
@@ -27,11 +54,11 @@ app.use("/api/order", require("./routes/order"));
 app.use("/api/product", require("./routes/product"));
 app.use("/api/user", require("./routes/user"));
 
-// last route
+
+// any other route
 app.get('*', function(req, res){
-    res.status(404).json({success: false, message: "Page not found"});
+  res.status(404).json({success: false, message: "Page not found"});
 });
 
-const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
